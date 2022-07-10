86f601d855864ff429f99de5f0118e4d2855dbfe
Roman Podobny <tazer16179@gmail.com>
2022-07-10 11:50:44 +0300

    last commit for video

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [precentBar, setPrecentBar] = useState('');
+    const [passInputChange, setPassInputChange] = useState('');
+    const [passInputClasses, setPassInputClasses] =
+        useState('pass-input-passive');
+    const [toggleIcon, setToggleIcon] = useState('☠️');
+    const [toggleIconClasses, setToggleIconClasses] = useState(
+        'toggle-icon-passive'
+    );
+    const [ripple, setRipple] = useState('');
+    const [passLabel, setPassLabel] = useState('Strength');
+    const [type, setType] = useState('password');
+
+    const addClass = (className) => {
+        setPrecentBar('');
+        if (className) {
+            setPrecentBar(className);
+        }
+    };
+
+    const handlePassInput = (e) => {
+        setPassInputChange(e.target.value);
+        if (passInputChange.length === 0) {
+            setPassLabel('Strength');
+            addClass();
+        } else if (passInputChange.length <= 4) {
+            setPassLabel('Weak');
+            addClass('weak');
+        } else if (passInputChange.length <= 7) {
+            setPassLabel('Not Bad');
+            addClass('average');
+        } else {
+            setPassLabel('Strong');
+            addClass('strong');
+        }
+    };
+
+    const togglePassInput = (e) => {
+        if (type === 'password') {
+            setType('text');
+            setToggleIcon('💀');
+            setRipple('ripple-active');
+            setPassInputClasses('pass-input-active');
+            setToggleIconClasses('toggle-icon-active');
+        } else {
+            setType('password');
+            setToggleIcon('☠️');
+            setRipple('ripple-passive');
+            setPassInputClasses('pass-input-passive');
+            setToggleIconClasses('toggle-icon-passive');
+        }
+    };
+    return (
+        <div className='App'>
+            <div className='input-container'>
+                <div className='input-group'>
+                    <input
+                        type={type}
+                        className={passInputClasses}
+                        placeholder='Enter your password'
+                        value={passInputChange}
+                        onChange={handlePassInput}
+                    />
+                    <span
+                        onClick={togglePassInput}
+                        className={`toggle ${toggleIconClasses}`}
+                    >
+                        {toggleIcon}
+                    </span>
+                    <span className={`ripple ${ripple}`}></span>
+                </div>
+                <div className='pass-strength'>
+                    <div className='strength-percent'>
+                        <span className={precentBar}></span>
+                    </div>
+                    <span className='strength-label'>{passLabel}</span>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
